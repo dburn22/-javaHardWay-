37190ec7ce4fd040892c7e33524cef0286cda86d
David Burnett <davidburnett@Davids-MacBook-Pro-3.local>
2017-01-09 10:52:44 -0700

    add exersice 1

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+	public static void main( String[] args ) {
+	System.out.println( "I David Burnett, I'm determined to learn how to code. " );
+	System.out.println( "Today's date is January 9th 2017" );
+	}
+}
