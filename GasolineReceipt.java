9673801fcfb668384dbed18643f08c288b29b5ed
David Burnett <davidburnett@Davids-MacBook-Pro-3.local>
2017-01-09 11:39:40 -0700

    add exercise 2

--- GasolineReceipt.java
+++ GasolineReceipt.java
@@ -1,17 +1,19 @@
 public class GasolineReceipt {
 	public static void main( String[] args ) {
 	System.out.println( "+-----------------------------+" );
-	System.out.println( "|							   |" );
-	System.out.println( "|		Corner Store		   |" );
-	System.out.println( "|							   |" );
-	System.out.println( "|  2017-01-09  4:38PM		   |" );
-	System.out.println( "|							   |" );
-	System.out.println( "|	Gallons:		10.870	   |" );
-	System.out.println( "|	Price/gallon:	$2.089	   |" );
-	System.out.println( "| 							   |" );
-	System.out.println( "|	Fuel Total:		$22.71	   |" );
-	System.out.println( "|							   |" );
-	System.out.println( "|_____________________________|" );
+	System.out.println( "|                             |" );
+	System.out.println( "|      Corner Store           |" );
+	System.out.println( "|                             |" );
+	System.out.println( "|    2017-01-09 4:38PM        |" );
+	System.out.println( "|                             |" );
+	System.out.println( "|  Gallons:       10.870      |" );
+	System.out.println( "|  Price/gallon:  $2.089      |" );
+	System.out.println( "|                             |" );
+	System.out.println( "|  Fuel Total:    $22.71      |" );
+	System.out.println( "|                             |" );
+	System.out.println( "|-----------------------------|" );
+
+	
 	}
 }
 	
