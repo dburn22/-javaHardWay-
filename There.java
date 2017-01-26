61cfd55f2f2e7b8964656958dd3a519fe6b160ba
Dburn22 <david.burnett2@yahoo.com>
2017-01-26 11:47:36 -0700

    add exercise 33

---
+++ There.java
@@ -0,0 +1,16 @@
+public class There {
+	public static void main( String[] args ) {
+		System.out.println( "Here." );
+		erebor(); //By taking out the parens the program doesn't compile because erebor
+		//is not a known comand in java unless we put parens next to it and turn it into 
+		//a function
+		System.out.println( "Back first time. " );
+		erebor();
+		System.out.println( "Back second time. " );
+	}
+	
+	public static void //erebor() {
+		System.out.println( "There." );
+		
+		}
+	}
